ad2288b326aae5934a9d077f1c063619e69185bc
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-07-05 13:47:49 -0600

    Delete AG directory

--- AG/selecindividuos.m
+++
@@ -1,8 +0,0 @@
-function idx = selecindividuos(pob)
-% pob, población de individuos
-
-    % Seleccionar los dos mejores individuos de la población
-    [~,idx] = sort(pob(:,end),'descend');
-    idx = idx(1:2);
-
-end
